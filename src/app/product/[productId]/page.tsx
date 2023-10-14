69424d8911882ebc9217347160d17821cdaeb3cf
alicja.galkiewicz <alicja.galkiewicz@htdevelopers.com>
2023-10-14 14:33:27 +0200

    Create "add to cart" button, trigger server action

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -21,24 +21,36 @@ export async function generateMetadata({
 
 export default async function ProductDetailsPage({
 	params: { productId },
+	params,
 }: {
 	params: { productId: string };
 }) {
 	const product = await getProduct(productId);
 	const { image, name, price, description, category } = product;
 
+	async function addProductToCartAction() {
+		"use server";
+		console.log("productId", params.productId);
+	}
+
 	return (
 		<>
 			<div className="m-auto mb-12 flex gap-14">
 				{image && <ProductPhoto src={image.src} alt={image.alt} big />}
-				<div className="w-96">
+				<form className="w-96" action={addProductToCartAction}>
 					<div className="flex justify-between">
 						<h1 className="font-semibold text-slate-700">{name}</h1>
 						<ProductPrice price={price} />
 					</div>
 					<ProductCategory category={category} />
 					<p className="mt-2 text-xs text-slate-600">{description}</p>
-				</div>
+					<button
+						type="submit"
+						className="mt-8 w-full rounded-md border bg-slate-700 px-8 py-3 text-white"
+					>
+						Add to cart
+					</button>
+				</form>
 			</div>
 			<RelatedProducts categoryName={category} excludedId={productId} />
 		</>
